5a2c673a2f0a9c358e07240cb5fdebdd763f5f95
reconroy <codewithroy22@gmail.com>
2024-11-30 21:01:52 +0530

    swagger setup completed

--- routes/diffRoute.js
+++ routes/diffRoute.js
@@ -2,6 +2,37 @@ const express = require('express');
 const calculateDiff = require('../services/diffService');
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Diff
+ *   description: Code difference comparison endpoints
+ * 
+ * /api/diff:
+ *   post:
+ *     tags: [Diff]
+ *     summary: Compare two code snippets
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - oldCode
+ *               - newCode
+ *             properties:
+ *               oldCode:
+ *                 type: string
+ *               newCode:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Difference comparison successful
+ *       500:
+ *         description: Server error
+ */
+
 router.post('/', (req, res) => {
   const { original, modified } = req.body;
   try {
